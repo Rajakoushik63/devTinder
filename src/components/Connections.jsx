37ddbdc666fa9d654221b0c82a5c0a29017eef17
rajak <rajakoushik8008@gmail.com>
2025-01-23 11:49:38 +0530

    added peramanent clear chat and updated connections components

--- src/components/Connections.jsx
+++ src/components/Connections.jsx
@@ -74,3 +74,85 @@ const Connections = () => {
 };
 
 export default Connections;
+
+// import axios from "axios";
+// import { BASE_URL } from "../utils/constants";
+// import { useEffect, useCallback, useState } from "react";
+// import { useDispatch, useSelector } from "react-redux";
+// import { addConnections } from "../utils/connectionSlice";
+// import { Link } from "react-router-dom";
+
+// const Connections = () => {
+//   const connections = useSelector((store) => store.connections);
+//   const dispatch = useDispatch();
+//   const [error, setError] = useState("");
+
+//   const fetchConnections = useCallback(async () => {
+//     try {
+//       const res = await axios.get(BASE_URL + "/user/connections", {
+//         withCredentials: true,
+//       });
+//       dispatch(addConnections(res.data.data));
+//     } catch (err) {
+//       let errorMessage = err.response.data;
+//       setError(errorMessage);
+//     }
+//   }, [dispatch]);
+
+//   useEffect(() => {
+//     fetchConnections();
+//   }, [fetchConnections]);
+
+//   if (!connections) return null;
+//   if (connections.length === 0)
+//     return (
+//       <h1 className="flex justify-center font-bold text-2xl text-white">
+//         Connections Not Found
+//       </h1>
+//     );
+
+//   return (
+//     <div
+//       className="text-center mt-10 pb-20 bg-cover bg-no-repeat"
+//       style={{
+//         backgroundImage:
+//           "url(https://static.vecteezy.com/system/resources/thumbnails/036/214/430/small_2x/ai-generated-abstract-technology-backgrounds-in-blue-futuristic-communication-connection-generated-by-ai-photo.jpg)",
+//       }}
+//     >
+//       <h1 className="font-bold text-2xl p-2 bg-slate-500 rounded-md text-white">
+//         Connections
+//       </h1>
+//       {error && <p className="text-red-500">{error}</p>}
+//       {connections.map((connection) => {
+//         const { _id, firstName, lastName, photourl, age, gender, about } =
+//           connection;
+//         return (
+//           <div
+//             className="flex m-4 p-4 rounded-lg bg-base-300 w-1/2 mx-auto justify-between items-center"
+//             key={_id}
+//           >
+//             <div className="flex items-center">
+//               <img
+//                 alt="photo"
+//                 className="w-20 h-20 rounded-lg"
+//                 src={photourl}
+//               />
+//               <div className="text-left mx-4">
+//                 <h2 className="font-bold text-xl">
+//                   {firstName + " " + lastName}
+//                 </h2>
+//                 {age && gender && <p>{age + ", " + gender}</p>}
+//                 <p>{about}</p>
+//               </div>
+//             </div>
+//             <Link to={"/chat/" + _id}>
+//               <button className="btn btn-primary">Chat</button>
+//             </Link>
+//           </div>
+//         );
+//       })}
+//     </div>
+//   );
+// };
+
+// export default Connections;
